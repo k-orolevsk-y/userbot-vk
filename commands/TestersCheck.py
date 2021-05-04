bd43e3fa6c8278edd63c7a3aa9617b708e6966cd
k-orolevsk-y <i@korolevsky.me>
2021-05-04 14:39:25 +0300

    Переписал /tc, под своё API. Находиться в /testpool теперь нет необходимости + выводит новые дополнительные данные.

--- commands/TestersCheck.py
+++ commands/TestersCheck.py
@@ -1,4 +1,6 @@
+import ujson
 import config
+import requests
 import functions
 
 
@@ -20,32 +22,32 @@ def cmd(api, message, args, owner_id):
         )
         return
 
-    try:
-        tester_bool = api.groups.is_member(group_id=134304772, user_id=target['id'])
-    except:
+    tester_info = ujson.decode(requests.get(f"https://ssapi.ru/vk-bugs-api/?method=getReporter&reporter_id={target['id']}").text)
+    if tester_info.get('error') is True:
         api.messages.send(
-            random_id=0,
-            peer_id=message['peer_id'],
-            message=f"{config.prefixes['error']} Ограничение на проверку со стороны ВКонтакте.",
-            reply_to=message['id']
-        )
-        return
-
-    if not tester_bool:
-        api.messages.send(
-            peer_id=message['peer_id'],
-            random_id=0,
-            message=f"{config.prefixes['success_no']} [id{target['id']}|{target['first_name']} {target['last_name']}] не тестировщик из [testpool|/testpool].",
-            reply_to=message['id'],
-            disable_mentions=True
+                peer_id=message['peer_id'],
+                random_id=0,
+                message=f"{config.prefixes['error']} Ошибка при обращении к API: {tester_info.get('error_msg')}",
+                reply_to=message['id'],
+                disable_mentions=True
         )
     else:
-        api.messages.send(
-            peer_id=message['peer_id'],
-            random_id=0,
-            message=f"{config.prefixes['success']} [id{target['id']}|{target['first_name']} {target['last_name']}] тестировщик из [testpool|/testpool].\n\nhttps://vk.com/bugs?act=reporter&id={target['id']}",
-            reply_to=message['id'],
-            disable_mentions=True
-        )
+        tester_info = tester_info.get('response').get('reporter')
+        if not tester_info.get('tester'):
+            api.messages.send(
+                peer_id=message['peer_id'],
+                random_id=0,
+                message=f"{config.prefixes['success_no']} [id{target['id']}|{target['first_name']} {target['last_name']}] не тестировщик из [testpool|/testpool].",
+                reply_to=message['id'],
+                disable_mentions=True
+            )
+        else:
+            api.messages.send(
+                peer_id=message['peer_id'],
+                random_id=0,
+                message=f"{config.prefixes['success']} [id{target['id']}|{target['first_name']} {target['last_name']}] тестировщик из [testpool|/testpool].\n\n• {tester_info.get('status_text')}\n• Количество отчётов: {tester_info.get('reports_count')}\n• Место в топе: {tester_info.get('top_position')}\n\nhttps://vk.com/bugs?act=reporter&id={target['id']}",
+                reply_to=message['id'],
+                disable_mentions=True
+            )
 
     return
